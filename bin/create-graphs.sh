f2dd5a642378605d272f019cdf8610384ff930cd
Brian Masney <masneyb@onstation.org>
2016-07-16 12:28:09 -0400

    put wind speed average and gusts on the same graph; battery and booster are also on the same graph

--- bin/create-graphs.sh
+++ bin/create-graphs.sh
@@ -2,50 +2,50 @@
 
 YADL_BIN_DIR=/home/masneyb/data/pi-yadl/bin
 CREATE_MIN_MAX_GRAPHS="${YADL_BIN_DIR}"/create-min-max-graphs.sh
+CREATE_MAX_GRAPHS="${YADL_BIN_DIR}"/create-max-graphs.sh
 WEB_BASE_DIR=/home/masneyb/data/weather-station/web
 
-"${YADL_BIN_DIR}"/create-temperature-humidity-graphs.sh \
-	"${WEB_BASE_DIR}"/temperature_humidity "Temperature and Humidity"
-
 "${CREATE_MIN_MAX_GRAPHS}" \
-	"${WEB_BASE_DIR}"/argent_80422.rrd \
-	wind_speed_cur \
+	"${WEB_BASE_DIR}"/temperature_humidity \
+	"Temperature and Humidity" \
+	"Temperature" \
+	"${WEB_BASE_DIR}"/temperature_humidity.rrd \
+	temperature \
+	"Relative Humidity" \
+	"${WEB_BASE_DIR}"/temperature_humidity.rrd \
+	humidity
+
+"${CREATE_MAX_GRAPHS}" \
 	"${WEB_BASE_DIR}"/wind_speed \
-	"Wind Speed (mph)"
+	"Wind Speed" \
+	"Wind Speed Average" \
+	"${WEB_BASE_DIR}"/argent_80422.rrd \
+	wind_speed_avg_2m \
+	"Wind Speed Gust" \
+	"${WEB_BASE_DIR}"/argent_80422.rrd \
+	wind_speed_gust_2m
 
 "${CREATE_MIN_MAX_GRAPHS}" \
-	"${WEB_BASE_DIR}"/argent_80422.rrd \
-	wind_speed_gust_2m \
-	"${WEB_BASE_DIR}"/wind_speed_gust_2m \
-	"Wind Speed Gust - 2 mins (mph)"
-
-"${CREATE_MIN_MAX_GRAPHS}" \
-	"${WEB_BASE_DIR}"/argent_80422.rrd \
-	wind_dir_cur \
-	"${WEB_BASE_DIR}"/wind_direction \
-	"Wind Direction (degrees)"
-
-"${CREATE_MIN_MAX_GRAPHS}" \
-	"${WEB_BASE_DIR}"/argent_80422.rrd \
-	rain_gauge_cur \
 	"${WEB_BASE_DIR}"/rain_gauge \
-	"Rain Gauge (in)"
+	"Rain Gauge (in)" \
+	"Rain Gauge (in)" \
+	"${WEB_BASE_DIR}"/argent_80422.rrd \
+	rain_gauge_cur
 
 "${CREATE_MIN_MAX_GRAPHS}" \
-	"${WEB_BASE_DIR}"/bmp180.rrd \
-	pressure_in \
 	"${WEB_BASE_DIR}"/pressure_in \
-	"Pressure (in)"
+	"Pressure (in)" \
+	"Pressure (in)" \
+	"${WEB_BASE_DIR}"/bmp180.rrd \
+	pressure_in
 
 "${CREATE_MIN_MAX_GRAPHS}" \
+	"${WEB_BASE_DIR}"/battery \
+	"Voltages" \
+	"Battery Charge (mV)" \
 	"${WEB_BASE_DIR}"/battery.rrd \
 	millivolts \
-	"${WEB_BASE_DIR}"/battery \
-	"Battery Charge (mV)"
-
-"${CREATE_MIN_MAX_GRAPHS}" \
+	"Booster (mV)" \
 	"${WEB_BASE_DIR}"/booster.rrd \
-	millivolts \
-	"${WEB_BASE_DIR}"/booster \
-	"Booster (mV)"
+	millivolts
 
