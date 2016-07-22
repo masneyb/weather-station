ad0ba46d0d7b6d761d6c2f53af1698c19ac2b32c
Brian Masney <masneyb@onstation.org>
2016-07-22 19:58:24 -0400

    optionally publish the results to weather underground after the readings have been gathered. This requires putting your station's ID and password in the systemd unit file.

--- bin/gather-data.sh
+++ bin/gather-data.sh
@@ -2,16 +2,23 @@
 
 YADL_BIN="${1:-}"
 WEB_BASE_DIR="${2:-}"
+ID="${3:-}"
+PASSWORD="${4:-}"
 
 if [ "${YADL_BIN}" = "" ] || [ "${WEB_BASE_DIR}" = "" ] ; then
-        echo "usage: $0 <path to yadl binary> <path to web/ directory" >&2
+        echo "usage: $0 <path to yadl binary> <path to web/ directory [ <weather underground ID> <weather underground password> ]" >&2
         exit 1
 fi
 
-"${YADL_BIN}" --gpio_pin 3 --sensor dht22 --temperature_unit fahrenheit --output rrd --outfile "${WEB_BASE_DIR}"/temperature_humidity.rrd --output single_json --outfile "${WEB_BASE_DIR}"/temperature_humidity.json
+"${YADL_BIN}" --gpio_pin 3 --sensor dht22 --temperature_unit fahrenheit --max_retries 30 --output rrd --outfile "${WEB_BASE_DIR}"/temperature_humidity.rrd --output single_json --outfile "${WEB_BASE_DIR}"/temperature_humidity.json
 
 "${YADL_BIN}" --sensor bmp180 --i2c_device /dev/i2c-1 --i2c_address 77 --temperature_unit fahrenheit --output rrd --outfile "${WEB_BASE_DIR}"/bmp180.rrd --output single_json --outfile "${WEB_BASE_DIR}"/bmp180.json
 
 "${YADL_BIN}" --sensor analog --adc mcp3008 --spi_channel 0 --analog_channel 3 --adc_millivolts 5000 --output rrd --outfile "${WEB_BASE_DIR}"/battery.rrd --output single_json --outfile "${WEB_BASE_DIR}"/battery.json
 
 "${YADL_BIN}" --sensor analog --adc mcp3008 --spi_channel 0 --analog_channel 2 --adc_millivolts 5000 --output rrd --outfile "${WEB_BASE_DIR}"/booster.rrd --output single_json --outfile "${WEB_BASE_DIR}"/booster.json
+
+if [ "${ID}" != "" ] && [ "${PASSWORD}" != "" ] ; then
+	/home/masneyb/data/weather-station/bin/weather-underground-publish.sh "${WEB_BASE_DIR}" "${ID}" "${PASSWORD}"
+fi
+
