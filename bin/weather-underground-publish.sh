c9fd29ce81b914f8d8f9f1b344f619caa1f76d5d
Brian Masney <masneyb@onstation.org>
2016-07-29 11:01:56 -0400

    don't send readings to weather underground that are not current. Only publish what is currently available.

--- bin/weather-underground-publish.sh
+++ bin/weather-underground-publish.sh
@@ -26,11 +26,13 @@ validate_timestamp()
 	DIFF=$((NOW - TS))
 	if [ "${DIFF}" -gt 300 ] ; then
 		echo "Timestamp ${TS} is ${DIFF} seconds from now and is too old to publish." >&2
-		exit 1
+		return 1
 	elif [ "${DIFF}" -lt 0 ] ; then
 		echo "Timestamp ${TS} is ${DIFF} seconds in the future and cannot be published." >&2
-		exit 1
+		return 1
 	fi
+
+	return 0
 }
 
 display_number_in_url_field()
@@ -50,26 +52,39 @@ generate_url()
 {
 	TMP=$(mktemp)
 
-	jq -r '.result[]|[.temperature, .humidity, .dew_point, .timestamp]|@csv' < "${WEB_BASE_DIR}"/temperature_humidity.json > "${TMP}"
+	# See http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
+	# for the supported URL parameters.
 
+	echo -n "${BASE_URL}?softwaretype=tinyurl.com%2Fhyft8kp&dateutc=now&action=updateraw&ID=${ID}&PASSWORD=${PASSWORD}"
+
+	NUM_OK=0
+	jq -r '.result[]|[.temperature, .humidity, .dew_point, .timestamp]|@csv' < "${WEB_BASE_DIR}"/temperature_humidity.json > "${TMP}"
 	TEMPF=$(awk -F, '{print $1}' < "${TMP}")
 	HUMIDITY=$(awk -F, '{print $2}' < "${TMP}")
 	DEWPTF=$(awk -F, '{print $3}' < "${TMP}")
 	TS=$(awk -F, '{print $4}' < "${TMP}")
-
 	validate_timestamp "${TS}"
-
+	if [ $? = 0 ] ; then
+		NUM_OK=$((NUM_OK + 1))
+		display_number_in_url_field tempf "${TEMPF}"
+		display_number_in_url_field humidity "${HUMIDITY}"
+		display_number_in_url_field dewptf "${DEWPTF}"
+	else
+		echo "Warning: Temperature / humidity readings are not current. Not sending these values." >&2
+	fi
 
 	jq -r '.result[]|[.pressure_in, .timestamp]|@csv' < "${WEB_BASE_DIR}"/bmp180.json > "${TMP}"
-
 	BAROMIN=$(awk -F, '{print $1}' < "${TMP}")
 	TS=$(awk -F, '{print $2}' < "${TMP}")
-
 	validate_timestamp "${TS}"
-
+	if [ $? = 0 ] ; then
+		NUM_OK=$((NUM_OK + 1))
+		display_number_in_url_field baromin "${BAROMIN}"
+	else
+		echo "Warning: Barometer reading is not current. Not sending this value." >&2
+	fi
 
 	jq -r '.result[]|[.rain_gauge_1h, .wind_dir_cur, .wind_speed_cur, .wind_speed_avg_2m, .wind_dir_avg_2m, .wind_speed_gust_10m, .wind_dir_gust_10m, .wind_speed_gust_2m, .wind_dir_gust_2m, .timestamp]|@csv' < "${WEB_BASE_DIR}"/argent_80422.json > "${TMP}"
-	
 	RAININ=$(awk -F, '{print $1}' < "${TMP}")
 	WINDDIR=$(awk -F, '{print $2}' < "${TMP}")
 	WINDSPEEDMPH=$(awk -F, '{print $3}' < "${TMP}")
@@ -80,29 +95,30 @@ generate_url()
 	WINDGUSTMPH_2M=$(awk -F, '{print $8}' < "${TMP}")
 	WINDGUSTDIR_2M=$(awk -F, '{print $9}' < "${TMP}")
 	TS=$(awk -F, '{print $10}' < "${TMP}")
-
 	validate_timestamp "${TS}"
+	if [ $? = 0 ] ; then
+		NUM_OK=$((NUM_OK + 1))
+		display_number_in_url_field rainin "${RAININ}"
+		display_number_in_url_field winddir "${WINDDIR}"
+		display_number_in_url_field windspeedmph "${WINDSPEEDMPH}"
+		display_number_in_url_field windgustdir "${WINDGUSTDIR_2M}"
+		display_number_in_url_field windgustmph "${WINDGUSTMPH_2M}"
+		display_number_in_url_field windspdmph_avg2m "${WINDSPDMPH_AVG2M}"
+		display_number_in_url_field winddir_avg2m "${WINDDIR_AVG2M}"
+		display_number_in_url_field windgustmph_10m "${WINDGUSTMPH_10M}"
+		display_number_in_url_field windgustdir_10m "${WINDGUSTDIR_10M}"
+	else
+		echo "Warning: Wind / rain readings are not current. Not sending these values." >&2
+	fi
 
-	# See http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
-	# for the supported URL parameters.
-
-	echo -n "${BASE_URL}?softwaretype=tinyurl.com%2Fhyft8kp&dateutc=now&action=updateraw&ID=${ID}&PASSWORD=${PASSWORD}"
-	display_number_in_url_field tempf "${TEMPF}"
-	display_number_in_url_field humidity "${HUMIDITY}"
-	display_number_in_url_field dewptf "${DEWPTF}"
-	display_number_in_url_field baromin "${BAROMIN}"
-	display_number_in_url_field rainin "${RAININ}"
-	display_number_in_url_field winddir "${WINDDIR}"
-	display_number_in_url_field windspeedmph "${WINDSPEEDMPH}"
-	display_number_in_url_field windgustdir "${WINDGUSTDIR_2M}"
-	display_number_in_url_field windgustmph "${WINDGUSTMPH_2M}"
-	display_number_in_url_field windspdmph_avg2m "${WINDSPDMPH_AVG2M}"
-	display_number_in_url_field winddir_avg2m "${WINDDIR_AVG2M}"
-	display_number_in_url_field windgustmph_10m "${WINDGUSTMPH_10M}"
-	display_number_in_url_field windgustdir_10m "${WINDGUSTDIR_10M}"
 	echo ""
 
 	rm -f "${TMP}"
+
+	if [ "${NUM_OK}" = 0 ] ; then
+		return 1
+	fi
+	return 0
 }
 
 WEB_BASE_DIR="${1:-}"
@@ -111,7 +127,7 @@ PASSWORD="${3:-}"
 BASE_URL="${4:-}"
 
 if [ "${WEB_BASE_DIR}" = "" ] || [ "${ID}" = "" ] || [ "${PASSWORD}" = "" ] ; then
-        echo "usage: $0 <path to web/ directory <wunderground ID> <wunderground password> [ <wunderground base URL> ]" >&2
+        echo "usage: $0 <path to web/ directory> <wunderground ID> <wunderground password> [ <wunderground base URL> ]" >&2
         exit 1
 fi
 
@@ -120,8 +136,10 @@ if [ "${BASE_URL}" = "" ] ; then
 fi
 
 URL=$(generate_url)
-
-echo "Generated URL ${URL}"
-
-curl "${URL}"
+if [ $? = 0 ] ; then
+	echo "Generated URL ${URL}"
+	curl "${URL}"
+else
+	echo "Error: No readings are current; not publishing to Weather Underground" >&2
+fi
 
